9f61fc8757b5662761faf2d47c14a732e545fb8c
EberMayta <86860213+EberMayta@users.noreply.github.com>
2021-07-09 16:32:26 -0500

    Delete PEA3_COBRANZA directory

--- PEA3_COBRANZA/src/main/java/com/init/cobranzas/dao/DeudaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.init.cobranzas.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.init.cobranzas.entidad.Deudas;
-
-public interface DeudaDAO  extends JpaRepository<Deudas, Integer>{
-
-}
--- PEA3_COBRANZA/src/main/java/com/init/cobranzas/dao/PagoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.init.cobranzas.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.init.cobranzas.entidad.Pagos;
-
-public interface PagoDAO extends JpaRepository<Pagos, Integer> {
-
-}
